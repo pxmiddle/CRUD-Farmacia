17aca154e7960bcd9b64e23f68b87aa8a74eaca3
Pablo Henrique <pablo.h.s.maia@gmail.com>
2024-01-04 11:24:03 -0300

    CRUD Categoria completo

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,44 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 100)
+	@NotBlank(message = "O Atributo Tipo é obrigatório")
+	@Size(min = 5, max = 100, message = "O Atributo Tipo deve conter no mínimo 5 e no maximo 100 caractéres")
+	private String tipo;
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	
+}
