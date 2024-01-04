9b875d18e889e96ada8f70091724edfd8d1499ab
Pablo Henrique <pablo.h.s.maia@gmail.com>
2024-01-04 12:03:36 -0300

    CRUD-Produto concluido

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,10 +1,17 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -22,6 +29,10 @@ public class Categoria {
 	@Size(min = 5, max = 100, message = "O Atributo Tipo deve conter no mínimo 5 e no maximo 100 caractéres")
 	private String tipo;
 
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public String getTipo() {
 		return tipo;
@@ -37,6 +48,14 @@ public class Categoria {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 
 
